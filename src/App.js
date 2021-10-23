d347b3497e498c199cebbb2043945e0a432557fa
teru <shingekinoengineer@gmail.com>
2021-10-24 04:19:11 +0900

    Login画面（Stateがわからない）

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import liff from "@line/liff";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import TopPage from "./pages/TopPage";
 import CameraPage from "./pages/CameraPage";
@@ -11,23 +10,6 @@ import LoginPage from "./pages/LoginPage";
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
 
-  async function login() {
-    // LIFFの初期化
-    const liffId = "1656562501-GwVbApqE";
-    await liff.init({ liffId }).catch((err) => {
-      window.alert("LIFFの初期化失敗\n" + err);
-    });
-
-    // LINEに未認証の場合、ログイン画面にリダイレクト
-    if (!liff.isLoggedIn()) {
-      await liff.login();
-      return;
-    }
-
-    setLoggedIn(true);
-    // return <Redirect />;
-  }
-
   return (
     <BrowserRouter>
       <div>
@@ -36,7 +18,7 @@ function App() {
             {loggedIn ? <TopPage /> : <Redirect to="/login" />}
           </Route>
           <Route path="/login">
-            <LoginPage login={login} />
+            <LoginPage loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
           </Route>
           <Route path="/ar-reader">
             <ARReaderPage />
