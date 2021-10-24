8387f01c80070389f6d3afff323e8aabfaa44c59
teru <shingekinoengineer@gmail.com>
2021-10-24 14:06:29 +0900

    temporary

--- src/pages/ARReaderPage.js
+++ src/pages/ARReaderPage.js
@@ -1,5 +1,5 @@
 import Container from "@mui/material/Container";
-import Webcam from "react-webcam";
+// import Webcam from "react-webcam"
 import BackButton from "../components/BackButton";
 import styled from "styled-components";
 
@@ -7,14 +7,14 @@ function ARReaderPage() {
   return (
     <Container maxwidth="sm">
       <BackButton />
-      <StyledWebcam />
+      {/* <StyledWebcam /> */}
     </Container>
   );
 }
 
-const StyledWebcam = styled(Webcam)`
-  width: 100%;
-  height: 100%;
-`;
+// const StyledWebcam = styled(Webcam)`
+//   width: 100%;
+//   height: 100%;
+// `;
 
 export default ARReaderPage;
